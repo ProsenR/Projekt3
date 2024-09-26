7921e6e50aae926a1cd0bb661f692f5a9056686a
Prosen <rprosen@tsn.at>
2024-09-26 08:37:32 +0200

    Kürzen Abfrage

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,41 @@
 import javax.swing.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        class Bruch{
+        class Bruch {
 
-        int zähler;
-        int nenner;
+            int zähler;
+            int nenner;
+
+
+
 
         }
-//hallo
+
+        Scanner scanner = new Scanner(System.in);
+
 
         Bruch bruch1;
         bruch1 = new Bruch();
-        bruch1.zähler= 2;
-        bruch1.nenner = 3;
+        bruch1.zähler= 6;
+        bruch1.nenner = 8;
 
         JOptionPane.showInternalMessageDialog(null,"Bruch: " + bruch1.zähler+ " / " + bruch1.nenner);
 
-        Bruch bruch2;
-        bruch2 = new Bruch();
-        bruch2.zähler = 5;
-        bruch2.nenner = 7;
-        JOptionPane.showInternalMessageDialog(null,"Bruch: " + bruch2.zähler+ " / " + bruch2.nenner);
+        System.out.println("Möchtest du diesen Bruch gekürzt haben?");
+        String antwort = scanner.nextLine();
+
+        if (antwort.equals("J") || antwort.equals("j")){
+            System.out.println("Hier ist Ihr gekürzter Bruch: " + bruch1.zähler/2 + " / " + bruch1.nenner/2);
+        }
+        else if (antwort.equals("N")  || antwort.equals("n")) {
+            System.out.println("Alles klar");
+        }
+        else {
+            System.out.println("Ungültige Eingabe. Bitte geben Sie j oder n ein.");
+        }
 
     }
 }
